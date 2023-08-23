85ccce66ab65ab2df7655e5558299d2846d19267
root <incubator-it@protonmail.com>
2023-08-23 21:49:24 +0300

    typography-component#5

---
+++ src/components/ui/typography/typography.types.ts
@@ -0,0 +1,34 @@
+import { ElementType } from 'react'
+
+export type VariantStyle =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'body1'
+  | 'subtitle1'
+  | 'body2'
+  | 'subtitle2'
+  | 'caption'
+  | 'overline'
+  | 'link1'
+  | 'link2'
+  | 'large'
+
+type TagMapping = {
+  [key in VariantStyle]: ElementType
+}
+
+export const tagMapping: TagMapping = {
+  h1: 'h1',
+  h2: 'h2',
+  h3: 'h3',
+  body1: 'p',
+  subtitle1: 'sub',
+  body2: 'p',
+  subtitle2: 'sub',
+  caption: 'caption',
+  overline: 'span',
+  link1: 'a',
+  link2: 'a',
+  large: 'h1',
+}
