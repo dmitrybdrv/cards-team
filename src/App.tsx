76608611d7f18502aa577a19a53b4c7dcf93a15e
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-08-24 20:24:16 +0300

    slider ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,3 @@
-import { Button } from './components/ui'
-import { SliderApp } from './components/ui/slider'
-
 export function App() {
-  return (
-    <div>
-      <Button variant="primary" as="a" fullWidth={false}>
-        Hello
-      </Button>
-      <div>
-        <Button variant="secondary" fullWidth={false}>
-          hello2
-        </Button>
-      </div>
-      <div>
-        <SliderApp />
-      </div>
-    </div>
-  )
+  return <div>hello friend</div>
 }
