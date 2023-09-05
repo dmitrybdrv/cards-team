f47690ade259fb1f12858e3bb85c2a9c8eac98cc
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-09-05 21:25:20 +0300

    refactor completed

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,40 +1,53 @@
 import { FC } from 'react'
 
 import * as Checkbox from '@radix-ui/react-checkbox'
-import { CheckedState } from '@radix-ui/react-checkbox'
+import { clsx } from 'clsx'
 
 import s from './checkbox.module.scss'
 import { checkBoxReturner } from './checkbox.utils.tsx'
 
 type CheckBoxProps = {
-  defaultChecked: CheckedState
-  onChange: (checked: boolean) => void
   disabled?: boolean
   className?: string
+  label?: string
+  name?: string
+  id?: string
+  required?: boolean
+  checked: boolean
+  onChange: (checked: boolean) => void
 }
 
 export const CheckBox: FC<CheckBoxProps> = ({
-  defaultChecked,
   onChange,
   disabled = false,
   className,
+  checked,
+  label,
+  id,
   ...rest
 }) => {
-  const checkBoxContent = checkBoxReturner(defaultChecked, disabled)
+  const checkBoxContent = checkBoxReturner(checked, disabled)
 
-  const checkboxStyle = `${s.checkboxRoot} ${className}`
+  const checkboxStyle = clsx(s.checkboxRoot, className)
+  const htmlId = id || rest.name || Math.random().toString()
 
   return (
     <div className={s.checkBoxContainer}>
       <Checkbox.Root
         className={checkboxStyle}
-        defaultChecked={defaultChecked}
         onCheckedChange={onChange}
         disabled={disabled}
+        checked={checked}
+        id={htmlId}
         {...rest}
       >
         {checkBoxContent}
       </Checkbox.Root>
+      {label && (
+        <label className={s.label} htmlFor={htmlId}>
+          {label}
+        </label>
+      )}
     </div>
   )
 }
