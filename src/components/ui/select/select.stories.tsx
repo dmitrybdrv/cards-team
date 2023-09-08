2d4c0323e4fe840825dd4ce18ab6baf26c302de3
platonVV <platonvolodin@icloud.com>
2023-09-08 12:08:27 -0400

    copy

--- src/components/ui/select/select.stories.tsx
+++ src/components/ui/select/select.stories.tsx
@@ -1,1 +1,44 @@
-export default {}
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { SelectC } from './'
+
+const meta = {
+  title: 'Components/SelectC',
+  component: SelectC,
+  tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      options: ['primary', 'active', 'hover', 'focus'],
+      control: { type: 'radio' },
+    },
+  },
+} satisfies Meta<typeof SelectC>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    values: ['apple', 'orange', 'kiwi'],
+    variant: 'primary',
+  },
+}
+
+export const Active: Story = {
+  args: {
+    values: ['apple', 'orange', 'kiwi'],
+    variant: 'active',
+  },
+}
+export const Hover: Story = {
+  args: {
+    values: ['apple', 'orange', 'kiwi'],
+    variant: 'hover',
+  },
+}
+export const Focus: Story = {
+  args: {
+    values: ['apple', 'orange', 'kiwi'],
+    variant: 'focus',
+  },
+}
