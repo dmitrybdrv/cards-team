93ef2b360d7acab350cde3aed9537af6afcbff81
platonVV <platonvolodin@icloud.com>
2023-09-23 09:47:18 -0400

    radix implementation busLogic

--- src/components/ui/drop-down/drop-down.tsx
+++ src/components/ui/drop-down/drop-down.tsx
@@ -6,17 +6,20 @@ import s from './drop-down.module.scss'
 
 export const DropDown = () => {
   return (
-    <div className={s.dropDownContainer}>
+    <div className={s.container}>
       <DropdownMenu.Root>
         <DropdownMenu.Trigger className={s.trigger}>
           <img className={s.imgHeader} src={ava} alt={'user'} />
         </DropdownMenu.Trigger>
 
-        <DropdownMenu.Portal className={s.portal}>
+        <DropdownMenu.Portal>
           <DropdownMenu.Content className={s.content}>
-            <DropdownMenu.Item>Log out</DropdownMenu.Item>
-            <DropdownMenu.Item>Change ava</DropdownMenu.Item>
-            <DropdownMenu.Item>Test</DropdownMenu.Item>
+            <DropdownMenu.Item className={s.item}>Log out</DropdownMenu.Item>
+            <DropdownMenu.Item className={s.item}> Change ava</DropdownMenu.Item>
+            <DropdownMenu.Item className={s.item}>Test</DropdownMenu.Item>
+            <DropdownMenu.Arrow className={s.arrowBox}>
+              <div className={s.arrow} />
+            </DropdownMenu.Arrow>
           </DropdownMenu.Content>
         </DropdownMenu.Portal>
       </DropdownMenu.Root>
