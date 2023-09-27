23e1edf584d74d96028422df69d05f14951690bc
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-09-27 22:12:57 +0300

    add buttons show

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Pagination } from '@/components/ui/pagination/pagination.tsx'
 export function App() {
   return (
     <div>
-      <Pagination />
+      <Pagination totalItems={4} />
     </div>
   )
 }
--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -1,28 +1,43 @@
+import { FC, ReactNode } from 'react'
+
 import s from './pagination.module.scss'
 
 import { ReactComponent as ArrowLeft } from '@/assets/icons/arrowLeft.svg'
 import { SelectC, Typography } from '@/components/ui'
 
-export const Pagination = ({}) => {
+type Props = {
+  currentPage?: number
+  itemsPerPage?: number
+  totalPages?: number
+  totalItems?: number
+  maxCountShowBtn?: number
+}
+
+export const Pagination: FC<Props> = ({ totalItems, maxCountShowBtn = 5 }) => {
   const perPageCountVariant = ['10', '20', '30', '50', '100']
 
-  const onChangeHandle = (value: string) => console.log(value)
+  const onChangeSelectHandle = (value: string) => console.log(value)
+  const pageButtons: ReactNode[] = []
+  const minItems = maxCountShowBtn > totalItems ? totalItems : maxCountShowBtn
+
+  for (let i = 0; i < minItems; i++) {
+    pageButtons.push(<button>{i}</button>)
+  }
+  // new Array(5).fill(<button value={'1'} title={'1'} />)
 
   return (
     <div className={s.wrapper}>
-      <div>
-        <ArrowLeft className={s.arrowLeft} />
-      </div>
+      <ArrowLeft className={s.arrowLeft} />
+      <div className={s.pageButtons}>{pageButtons}</div>
+      <ArrowLeft className={s.arrowRight} />
       <div className={s.showPerPageWrapper}>
         <Typography variant={'body2'}>Показать</Typography>
-        {/*<div className={s.selectWrapper}>*/}
         <SelectC
           values={perPageCountVariant}
           startValue={perPageCountVariant[4]}
           className={s.select}
-          onValueChange={onChangeHandle}
+          onValueChange={onChangeSelectHandle}
         />
-        {/*</div>*/}
         <Typography variant={'body2'}>на странице</Typography>
       </div>
     </div>
