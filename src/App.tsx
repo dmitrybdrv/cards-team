2f2d36102529bc8df432cfc1e4644b4cac706b10
dmitrybdrv <incubator-it@protonmail.com>
2023-09-28 21:59:42 +0300

    modal-component compound component created part 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import { useState } from 'react'
 
 import { Modal } from '@/components/layout/forms'
-import { Button } from '@/components/ui'
 
 export function App() {
   const [open, setOpen] = useState(false)
 
-  return (
-    <Modal onOpenChange={setOpen} isOpen={open}>
-      <Modal.Trigger>
-        <Button>Open</Button>
-      </Modal.Trigger>
-
-      <Modal.Portal>
-        <Modal.Title>Some title</Modal.Title>
-        <Modal.Description>Description</Modal.Description>
-      </Modal.Portal>
-    </Modal>
-  )
+  return <Modal onOpenChange={setOpen} isOpen={open} />
 }
--- src/components/layout/forms/modal/modal.tsx
+++ src/components/layout/forms/modal/modal.tsx
@@ -1,55 +1,30 @@
-import { FC, ReactNode } from 'react'
+import { FC } from 'react'
 
 import * as Dialog from '@radix-ui/react-dialog'
 
 import s from './modal.module.scss'
 
+import { Button } from '@/components/ui'
+
 type Props = {
   isOpen: boolean
   onOpenChange: (open: boolean) => void
-  children: ReactNode
 }
 
-type ModalCompoundComponents = {
-  Trigger: FC<{ children: ReactNode }>
-  Portal: FC<{ children: ReactNode[] }>
-  Title: FC<{ children: ReactNode }>
-  Description: FC<{ children: ReactNode }>
-  Close: FC
-}
-
-export const Modal: FC<Props> & ModalCompoundComponents = ({ isOpen, onOpenChange, children }) => {
+export const Modal: FC<Props> = ({ onOpenChange, isOpen }) => {
   return (
-    <Dialog.Root defaultOpen={false} open={isOpen} onOpenChange={onOpenChange}>
-      {children}
+    <Dialog.Root open={isOpen} onOpenChange={onOpenChange}>
+      <Dialog.Trigger>
+        <Button>open</Button>
+      </Dialog.Trigger>
+      <Dialog.Portal className={s.modalContainer}>
+        <Dialog.Overlay className={s.dialogOverlay} />
+        <Dialog.Content className={s.dialogContent}>
+          <Dialog.Title>Title</Dialog.Title>
+          <Dialog.Description>Description</Dialog.Description>
+          <Dialog.Close className={s.IconButton} />
+        </Dialog.Content>
+      </Dialog.Portal>
     </Dialog.Root>
   )
-}
-
-Modal.Trigger = ({ children }) => {
-  return <Dialog.Trigger>{children}</Dialog.Trigger>
-}
-
-Modal.Portal = ({ children }) => {
-  return (
-    <Dialog.Portal className={s.dialogContent}>
-      <Dialog.Overlay className={s.dialogOverlay} />
-      <Dialog.Content>
-        <Dialog.Title>{children}</Dialog.Title>
-        <Dialog.Description>{children}</Dialog.Description>
-      </Dialog.Content>
-    </Dialog.Portal>
-  )
-}
-
-Modal.Title = ({ children }) => {
-  return <Dialog.Title className={s.dialogTitle}>{children}</Dialog.Title>
-}
-
-Modal.Description = ({ children }) => {
-  return <Dialog.Description className={s.dialogDescription}>{children}</Dialog.Description>
-}
-
-Modal.Close = () => {
-  return <Dialog.Close />
 }
