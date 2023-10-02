0607eb43f1ffea56949d8b5f0180ee3f9ee421c4
AnatoliyPologovoy <pologovoy89@mail.ru>
2023-10-02 07:43:14 +0300

    add first page, style for currentPage

--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -15,32 +15,43 @@ type Props = {
   maxCountShowBtn?: number
 }
 
-export const Pagination: FC<Props> = ({ maxCountShowBtn = 5, totalPages = 0 }) => {
+export const Pagination: FC<Props> = ({ maxCountShowBtn = 5, totalPages = 0, currentPage = 1 }) => {
   const perPageCountVariant = ['10', '20', '30', '50', '100']
 
   const onChangeSelectHandle = (value: string) => console.log(value)
   const pageButtons: ReactNode[] = []
   const showItemsCount = maxCountShowBtn > totalPages ? totalPages : maxCountShowBtn
 
-  for (let i = 1; i <= showItemsCount; i++) {
-    const buttonStyle = clsx(s.pageButton, i % 2 === 0 && s.even)
-
+  // если currentPage < maxCountShowBtn
+  for (let i = 2; i <= showItemsCount; i++) {
     pageButtons.push(
-      <button key={i} className={buttonStyle}>
+      <button key={i} className={s.pageButton}>
         {i}
-        {/*<Typography variant={'body2'}>{i}</Typography>*/}
       </button>
     )
   }
+  //Как сделать отрисовку кнопок за пределами maxCountShowBtn? С точками по бокам
+  //с помощью порций?
+
+  //is show first dots
+  const showFirstDots = currentPage > maxCountShowBtn && <div className={s.dots}>...</div>
+  // Styles
+  const firstPageStyle = clsx(s.firstPage, currentPage === 1 && s.currentPage)
 
   return (
     <div className={s.wrapper}>
       <div className={s.arrowWrapper}>
         <Arrow className={s.arrowLeft} />
       </div>
-      <div className={s.dots}>...</div>
+      {/*First page*/}
+      <div className={firstPageStyle}>1</div>
+      {/*First dots*/}
+      {showFirstDots}
+      {/*Buttons*/}
       <div className={s.pageButtons}>{pageButtons}</div>
+      {/*Last dots*/}
       <div className={s.dots}>...</div>
+      {/*Last page*/}
       <div className={s.totalPages}>{totalPages}</div>
       <div className={s.arrowWrapper}>
         <Arrow className={s.arrowRight} />
