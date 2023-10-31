d461557ca8dd7fd1ad1b2d7a5ced0751b3625d61
platonVV <platonvolodin@icloud.com>
2023-10-31 14:06:54 -0400

    reset-password done done

--- src/components/layout/forms/success-reset-password/success-reset-password.tsx
+++ src/components/layout/forms/success-reset-password/success-reset-password.tsx
@@ -7,23 +7,22 @@ import { Button, Card, Typography } from '@/components'
 export const SuccessResetPassword = () => {
   return (
     <div className={s.wrapper}>
-      <Card>
+      <Card className={s.formWrapper}>
         <div className={s.congrats}>
           <Typography variant={'large'}>Congratulations! </Typography>
         </div>
-        <div className={s.h2}>
-          <Typography variant={'h1'}>You have successfully reset your password.</Typography>
-        </div>
+        <Typography variant={'h1'} className={s.h2}>
+          You have successfully reset your password.
+        </Typography>
         <div>
-          <Typography variant={'h3'} className={s.h3}>
-            You can now log into your account with your new password. Click below to return to sign
-            in page.
-          </Typography>
           <Link to={'/auth/login'}>
             <Button fullWidth={true} className={s.btn}>
               <Typography variant={'subtitle2'}>Return to Login</Typography>
             </Button>
           </Link>
+          <Typography variant={'link1'} className={s.h3}>
+            You can now log into your account with your new password.
+          </Typography>
         </div>
       </Card>
     </div>
